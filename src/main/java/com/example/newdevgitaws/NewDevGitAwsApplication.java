547382787a2c97ca9e51b20aaf1cc40f9cd7da3b
Sadheera Madhushan <gitzdsproj@gmail.com>
2023-10-15 17:01:59 +0530

    third commit

--- src/main/java/com/example/newdevgitaws/NewDevGitAwsApplication.java
+++ src/main/java/com/example/newdevgitaws/NewDevGitAwsApplication.java
@@ -22,7 +22,7 @@ public class NewDevGitAwsApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Hello World!";
+        return "Hello Aws World!";
     }
 
 }
