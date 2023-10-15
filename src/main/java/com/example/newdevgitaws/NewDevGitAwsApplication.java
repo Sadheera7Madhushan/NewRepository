4de42ca15dfd762e378d7b9fcf1d2db15665fe8c
Sadheera Madhushan <gitzdsproj@gmail.com>
2023-10-15 15:24:28 +0530

    Initial commit

---
+++ src/main/java/com/example/newdevgitaws/NewDevGitAwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.newdevgitaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewDevGitAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewDevGitAwsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/newdevgitaws/NewDevGitAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newdevgitaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewDevGitAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
