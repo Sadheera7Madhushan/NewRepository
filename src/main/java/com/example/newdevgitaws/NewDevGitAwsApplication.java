db883ebecd2d889229ff31cc00bc47a21772a639
Sadheera Madhushan <gitzdsproj@gmail.com>
2023-10-15 16:12:01 +0530

    second commit

--- src/main/java/com/example/newdevgitaws/NewDevGitAwsApplication.java
+++ src/main/java/com/example/newdevgitaws/NewDevGitAwsApplication.java
@@ -2,12 +2,29 @@ package com.example.newdevgitaws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
-public class NewDevGitAwsApplication {
+public class NewDevGitAwsApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(NewDevGitAwsApplication.class);
+    }
 
     public static void main(String[] args) {
-        SpringApplication.run(NewDevGitAwsApplication.class, args);
+        SpringApplication.run(NewDevGitAwsApplication.class);
+    }
+
+    @RequestMapping("/")
+    String helloWorld() {
+        return "Hello World!";
     }
 
 }
+
+
