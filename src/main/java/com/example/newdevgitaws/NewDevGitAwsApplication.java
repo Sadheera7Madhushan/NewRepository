5914b9dfe0415d4c7c201c272a492b07b3df7534
Sadheera Madhushan <gitzdsproj@gmail.com>
2023-10-15 20:18:18 +0530

    seventh commits

--- src/main/java/com/example/newdevgitaws/NewDevGitAwsApplication.java
+++ src/main/java/com/example/newdevgitaws/NewDevGitAwsApplication.java
@@ -1,15 +1,28 @@
 package com.example.newdevgitaws;
 
-import MainClass.FirstJava;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
-public class NewDevGitAwsApplication {
+public class NewDevGitAwsApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(NewDevGitAwsApplication.class);
+    }
 
     public static void main(String[] args) {
-        // SpringApplication.run(NewDevGitAwsApplication.class);
-        FirstJava java = new FirstJava();
-        java.showName();
+        SpringApplication.run(NewDevGitAwsApplication.class);
+    }
+
+    @RequestMapping("/")
+    public String helloWorld() {
+        return "Hello World!";
     }
 
 }
