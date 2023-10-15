07a9803704867c3ad812bab8b64feeb3fe7663bb
Sadheera Madhushan <gitzdsproj@gmail.com>
2023-10-15 19:43:21 +0530

    sixth commits

---
+++ src/main/java/MainClass/FirstJava.java
@@ -0,0 +1,10 @@
+package MainClass;
+
+public class FirstJava {
+    private String name;
+
+    public void showName() {
+        System.out.println("My name:");
+    }
+
+}
--- src/main/java/com/example/newdevgitaws/NewDevGitAwsApplication.java
+++ src/main/java/com/example/newdevgitaws/NewDevGitAwsApplication.java
@@ -1,28 +1,15 @@
 package com.example.newdevgitaws;
 
-import org.springframework.boot.SpringApplication;
+import MainClass.FirstJava;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
-public class NewDevGitAwsApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(NewDevGitAwsApplication.class);
-    }
+public class NewDevGitAwsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NewDevGitAwsApplication.class);
-    }
-
-    @RequestMapping("/")
-    String helloWorld() {
-        return "Hello World!";
+        // SpringApplication.run(NewDevGitAwsApplication.class);
+        FirstJava java = new FirstJava();
+        java.showName();
     }
 
 }
